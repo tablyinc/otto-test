5e01434e55516ab311802e7044ab675384c6d8d8
Alex <12738821+amascolo@users.noreply.github.com>
2022-09-15 18:11:49 +0200

    refactored duplicate code for fuzzing data structures

--- tests/1_1_register.rs
+++ tests/1_1_register.rs
@@ -1,71 +1,16 @@
-#![allow(clippy::if_not_else, clippy::range_plus_one)]
+use otto::settable::Settable as Register;
 
-use otto::{crdt::Crdt, settable::Settable as Register, StateTest};
-use rand::{rngs::SmallRng, Rng, SeedableRng};
+use common::{fuzz, fuzz_short};
 
-fn test_register<T: StateTest>(rng: &mut impl Rng) {
-	let mut a = Crdt::new(T::gen(rng));
-	let mut b = a.clone();
-
-	println!("start: {:?}", *a);
-
-	for _ in 0..rng.gen_range(1..5) {
-		let instr = if a.instrs().len() == 0 || rng.gen_range(0..5) != 0 {
-			let instr = StateTest::gen_trivial_instr(&*a, rng).unwrap();
-			Crdt::instr_to_crdt_instr(&a, instr)
-		} else {
-			let mut undos = a.instrs();
-			let undo = rng.gen_range(0..undos.len());
-			undos.nth(undo).unwrap().inverse()
-		};
-		a.apply(instr);
-	}
-
-	for _ in 0..rng.gen_range(1..5) {
-		let instr = if b.instrs().len() == 0 || rng.gen_range(0..5) != 0 {
-			let instr = StateTest::gen_trivial_instr(&*b, rng).unwrap();
-			Crdt::instr_to_crdt_instr(&b, instr)
-		} else {
-			let mut undos = b.instrs();
-			let undo = rng.gen_range(0..undos.len());
-			undos.nth(undo).unwrap().inverse()
-		};
-		b.apply(instr);
-	}
-
-	let a_instrs = a.instrs().collect::<Vec<_>>();
-	let b_instrs = b.instrs().collect::<Vec<_>>();
-
-	println!("a: {:?}\nb: {:?}", a, b);
-
-	b.apply_multiple(a_instrs);
-	a.apply_multiple(b_instrs);
-
-	println!("end: {:?}\n", *a);
-
-	assert_eq!(a, b);
-}
+mod common;
 
 #[ignore]
 #[test]
-fn fuzz_register() {
-	let seed = rand::random();
-	println!("seed: {seed}");
-	let rng = &mut SmallRng::seed_from_u64(seed);
-	for i in 0..u64::MAX {
-		if i % 100_000 == 0 {
-			println!("{}", i);
-		}
-		test_register::<Register<u8>>(rng);
-	}
+fn fuzz_list() {
+	fuzz::<Register<u8>>();
 }
 
 #[test]
-fn fuzz_register_short() {
-	let seed = rand::random();
-	println!("seed: {seed}");
-	let rng = &mut SmallRng::seed_from_u64(seed);
-	for _ in 0..100 {
-		test_register::<Register<u8>>(rng);
-	}
+fn fuzz_short_list() {
+	fuzz_short::<Register<u8>>();
 }
